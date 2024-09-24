ad7c3da3f500bdac7651e8185cbd3cfc9214bb4d
shabah900 <mahazubalshabah@gmail.com>
2024-09-24 20:22:30 +0600

    donation maximum done

--- all.js/index.js
+++ all.js/index.js
@@ -1,6 +1,60 @@
 
-            // blog page  
+// blog page  
 
-function goToBlogPage(){
+function goToBlogPage() {
     window.location.href = '/blog.html';
 }
+// donate button for Noakhali 
+document.getElementById('btn-donate-noakhali').addEventListener('click', function (event) {
+    event.preventDefault();
+    const donateAmount = document.getElementById('donate-amount-noakhali').value;
+    const mainInitial = document.getElementById('main-initial').innerText;
+
+    const totalPresent = parseFloat(mainInitial) - parseFloat(donateAmount);
+
+    const noakhaliBeforeDonate = document.getElementById('noakhali-total-donate').innerText;
+    const noakhaliTotalDonate = parseFloat(noakhaliBeforeDonate) + parseFloat(donateAmount);
+    // update noakhali after donate 
+    document.getElementById('noakhali-total-donate').innerText = noakhaliTotalDonate;
+
+    //  update total  balance 
+    document.getElementById('main-initial').innerText = totalPresent;
+    document.getElementById('donate-amount-noakhali').value='' ;
+})
+
+
+// donate button for feni 
+document.getElementById('btn-donate-feni').addEventListener('click', function (event) {
+    event.preventDefault();
+    const donateAmount = document.getElementById('donate-amount').value;
+    const mainInitial = document.getElementById('main-initial').innerText;
+
+    const totalPresent = parseFloat(mainInitial) - parseFloat(donateAmount);
+
+    const feniBeforeDonate = document.getElementById('feni-total-donate').innerText;
+    const feniTotalDonate = parseFloat(feniBeforeDonate) + parseFloat(donateAmount);
+    // update feni after donate 
+    document.getElementById('feni-total-donate').innerText = feniTotalDonate;
+
+    //  update total  balance 
+    document.getElementById('main-initial').innerText = totalPresent;
+    document.getElementById('donate-amount').value='' ;
+})    
+
+// donate button for Quota 
+document.getElementById('btn-donate-quota').addEventListener('click', function (event) {
+    event.preventDefault();
+    const donateAmount = document.getElementById('donate-amount-quota').value;
+    const mainInitial = document.getElementById('main-initial').innerText;
+
+    const totalPresent = parseFloat(mainInitial) - parseFloat(donateAmount);
+
+    const quotaBeforeDonate = document.getElementById('quota-total-donate').innerText;
+    const quotaTotalDonate = parseFloat(quotaBeforeDonate) + parseFloat(donateAmount);
+    // update quota after donate 
+    document.getElementById('quota-total-donate').innerText = quotaTotalDonate;
+
+    //  update total  balance 
+    document.getElementById('main-initial').innerText = totalPresent;
+    document.getElementById('donate-amount-quota').value='' ;
+})
